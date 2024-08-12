b6c762c0366074cbbfe566260bbc36d7027a91c8
bhuriya12 <162349127+bhuriya12@users.noreply.github.com>
2024-08-12 19:30:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ import re
 import os
 
 bot = Client("bot",
-             bot_token= "6181361269:AAGPmZWMDYEcBrn8rxEFlEmc4UbEQNPwbNc",
+             bot_token= "7270419558:AAFl_wPWcKB053DqfyMjEgrOq1BxqqKjkNk",
              api_id=26451206,
              api_hash= "32984406271d6f3945bb536671b143a7")
 
